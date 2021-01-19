9464743b9870e3741b0fb7c820985be8e35a8e6a
Vonjy Andriah <vonjyandriah@live.com>
2021-01-19 23:34:34 +0300

    add

--- GUI.py
+++ GUI.py
@@ -57,7 +57,7 @@ root = tk.Tk()
 root.title("Maintenance Book")     # Add a title
 
 
-tree = ttk.Treeview(root, column=("c1", "c2", "c3","c4", "c5", "c6","c7", "c8" ), show='headings')
+tree = ttk.Treeview(root, column=("c1", "c2", "c3","c4", "c5", "c6","c7" ), show='headings')
 
 tree.column("#1", anchor=tk.CENTER)
 
